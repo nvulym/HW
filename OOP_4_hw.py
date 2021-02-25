47828a2ff3f246a63f9f87709753793d886370cc
nvulym <79202444+nvulym@users.noreply.github.com>
2021-02-25 20:27:38 +0300

    dell_atom and dell_bond

--- OOP_4_hw.py
+++ OOP_4_hw.py
@@ -37,29 +37,26 @@ class Molecule:
         neigh1[map2] = bond
         neigh2[map1] = bond
 
-    def show_atom(self):
+    def print_atom(self):
         return self._atoms
 
-    def show_bond(self):
+    def print_bond(self):
         return self._bonds
 
     # добавить метод : del_atom(map_), del_bond(map1, map2)
     def dell_atom(self, map_):
         if not isinstance(map_, int):
             raise TypeError
-        elif map_ in self._atoms:
-            self._atoms.pop(map_)  # удаляет атом по номеру, если есть в словаре
-            for neigh in self._bonds[map_]:
-                self._bonds[neigh].pop(map_)
-            self._bonds.pop(map_)  # удаляет связи, которые были связаны с этим атомом
-
+        self._atoms.pop(map_)  # удаляет атом по номеру, если есть в словаре
+        for neigh in self._bonds[map_]:
+            self._bonds[neigh].pop(map_)
+        self._bonds.pop(map_)  # удаляет связи, которые были связаны с этим атомом
 
     def dell_bond(self, map1, map2):
-        if not isinstance((map1, map2), int):
+        if not isinstance(map1, int) or not isinstance(map2, int):
             raise TypeError
-
-        self._bonds.pop(map1)
-        self._bonds.pop(map2)
+        self._bonds[map1].pop(map2)
+        self._bonds[map2].pop(map1)
 
 mol = Molecule()
 mol.add_atom(atom = 'C', map_ = 1)
@@ -69,8 +66,13 @@ mol.add_atom(atom = 'C', map_ = 3)
 mol.add_bond(2, 3, 1)
 mol.add_atom(atom = 'Cl', map_ = 4)
 mol.add_bond(1, 4, 1)
-print('Atoms before', mol.show_atom())
-print('Bonds before', mol.show_bond())
-mol.dell_atom(map_=0)
-print('Atoms after', mol.show_atom())
-print('Bonds after', mol.show_bond())
+# print('Atoms before', mol.print_atom())
+# print('Bonds before', mol.print_bond())
+# mol.dell_atom(map_=8)
+# print('Atoms after', mol.print_atom())
+# print('Bonds after', mol.print_bond())
+print('Atoms before', mol.print_atom())
+print('Bonds before', mol.print_bond())
+mol.dell_bond(map1=1, map2=4)
+print('Atoms after', mol.print_atom())
+print('Bonds after', mol.print_bond())
