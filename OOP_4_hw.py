697eedd62706a449daa732c57efd4544fbee45e7
nvulym <79202444+nvulym@users.noreply.github.com>
2021-03-04 20:05:34 +0300

    class IterBonds, class IterAtom, class Atom, class Bond

--- OOP_4_hw.py
+++ OOP_4_hw.py
@@ -12,19 +12,18 @@ class Molecule:
             raise ValueError
         elif map_ in self._atoms:
             raise KeyError
-        if not isinstance(atom, str):
+        if not isinstance(atom, Atom):
             raise TypeError
-        elif atom not in ('C', 'N', 'Cl', 'Br', 'F', 'I', 'O', 'S'):
-            raise ValueError
-        self._atoms[map_] = atom
+        # elif atom not in ('C', 'N', 'Cl', 'Br', 'F', 'I', 'O', 'S'):
+        #     raise ValueError
+        self._atoms[map_] = atom.get_symbol()
         self._bonds[map_] = {}
 
     def add_bond(self, map1, map2, bond):
-        if not isinstance(bond, int):
+        if not isinstance(bond, Bond):
             raise TypeError
-        if bond not in (1, 2, 3):
-            raise ValueError
-
+        # if bond not in (1, 2, 3):
+        #     raise ValueError
         # есть ли в селф.атом , что мап1 не равно мап2, что уже есть связь м\у этими атомами, что одноатомн мол-ла
         neigh1 = self._bonds[map1]
         neigh2 = self._bonds[map2]
@@ -33,9 +32,10 @@ class Molecule:
             raise KeyError
         if map1 in neigh2:  # что уже есть связь м\у этими атомами
             raise KeyError
-
-        neigh1[map2] = bond
-        neigh2[map1] = bond
+        # neigh1[map2] = bond
+        # neigh2[map1] = bond
+        neigh1[map2] = bond.get_valence()
+        neigh2[map1] = bond.get_valence()
 
     def print_atom(self):
         return self._atoms
@@ -105,42 +105,87 @@ class IterAtom:
 
 class Atom:
     def __init__(self, isotope: int = None):
-    # isotope >= 1
+        if not isinstance(isotope, (int, type(None))):
+            raise ValueError
+        elif isinstance(isotope, int):
+            if isotope < 1:
+                raise TypeError
         self._isotope = isotope
-
     def __eq__(self, other):
         return isinstance(self, type(other)) and self._isotope == other._isotope
 
+    def get_symbol(self):
+        if not isinstance(self._symbol, str):
+            raise TypeError
+        return self._symbol
 
 class C(Atom):
-    ...
+    def __init__(self, isotope: int = None):
+        super().__init__(isotope)
+        self._symbol = "C"
 
 
 class O(Atom):
-    ...
+    def __init__(self, isotope: int = None):
+        super().__init__(isotope)
+        self._symbol = "O"
 
 
 class N(Atom):
-    ...
+    def __init__(self, isotope: int = None):
+        super().__init__(isotope)
+        self._symbol = "N"
+
+class Cl(Atom):
+    def __init__(self, isotope: int = None):
+        super().__init__(isotope)
+        self._symbol = "Cl"
 
 
 class Bond:
-    # порядок связи в инит
-    ...
+    # порядок связи в init
+    def __init__(self, val):
+        if not isinstance(val, int):
+            raise TypeError
+        if val not in (1, 2, 3):
+            raise ValueError
+        self._val = val
 
+    def get_valence(self):
+        return self._val
 
-
-
-# Д/З класс IterAtoms который возвращает пару (номер атома, имя атома)
 
 mol = Molecule()
-mol.add_atom(atom = 'C', map_ = 1)
-mol.add_atom(atom = 'C', map_ = 2)
-mol.add_bond(1, 2, 1)
-mol.add_atom(atom = 'C', map_ = 3)
-mol.add_bond(2, 3, 1)
-mol.add_atom(atom = 'Cl', map_ = 4)
-mol.add_bond(1, 4, 1)
+mol.add_atom(C())
+mol.add_atom(C())
+mol.add_atom(C())
+mol.add_atom(Cl())
+print(mol.print_atom())
+# => {1: 'C', 2: 'C', 3: 'C', 4: 'Cl'}
+mol.add_bond(1, 2, Bond(1))
+mol.add_bond(2, 3, Bond(1))
+mol.add_bond(1, 4, Bond(1))
+print(mol.print_bond())
+# => {1: {2: 1, 4: 1}, 2: {1: 1, 3: 1}, 3: {2: 1}, 4: {1: 1}}
+for bonds in mol.iter_bonds():
+    print(bonds)
+# (1, 2)
+# (1, 4)
+# (2, 3)
+for atoms in mol.iter_atom():
+    print(atoms)
+# (1, 'C')
+# (2, 'C')
+# (3, 'C')
+# (4, 'Cl')
+
+# mol.add_atom(atom = 'C', map_ = 1)
+# mol.add_atom(atom = 'C', map_ = 2)
+# mol.add_bond(1, 2, 1)
+# mol.add_atom(atom = 'C', map_ = 3)
+# mol.add_bond(2, 3, 1)
+# mol.add_atom(atom = 'Cl', map_ = 4)
+# mol.add_bond(1, 4, 1)
 # print('Atoms before', mol.print_atom())
 # print('Bonds before', mol.print_bond())
 # mol.dell_atom(map_=8)
@@ -151,14 +196,4 @@ mol.add_bond(1, 4, 1)
 # mol.dell_bond(map1=1, map2=4)
 # print('Atoms after', mol.print_atom())
 # print('Bonds after', mol.print_bond())
-# for bonds in mol.iter_bonds():
-#     print(bonds)
-# (1, 2)
-# (1, 4)
-# (2, 3)
-for atoms in mol.iter_atom():
-    print(atoms)
-# (1, 'C')
-# (2, 'C')
-# (3, 'C')
-# (4, 'Cl')
+
